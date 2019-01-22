7c4df0ce47f7d98094643c8cee643d7f4bd11660
Lodis Perkins <hypnotoad35@gmail.com>
2019-01-22 12:52:28 -0600

    commit

---
+++ dependencies/mathlib/include/Matrix3.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "vector3.h"
+class Matrix3
+{
+private:
+	float grid[3][3];
+public:
+	Matrix3();
+	Matrix3(float a, float b, float c, float d, float e, float f, float g, float h, float i);
+	void rotate(float);
+	Matrix3 operator * (Matrix3);
+	Vector3 operator[] (int);
+	Vector3 operator * (Vector3);
+};
--- project2D/main.cpp
+++ project2D/main.cpp
@@ -1,5 +1,5 @@
 #include "Application2D.h"
-
+#include <vector2.h>
 #include "CDDS_BinaryTreesApp.h"
 int main() {
 	
@@ -8,7 +8,7 @@ int main() {
 
 	// initialise and loop
 	app->run("AIE", 1280, 720, false);
-
+	Vector2 thing;
 	// deallocation
 	delete app;
 
